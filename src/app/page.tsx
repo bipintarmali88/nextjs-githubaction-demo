5fcc3cb7075a37fbab64f95a02074eeaee8f8e74
bk250219 <bk250219@ncr.com>
2024-01-11 00:20:15 +0530

    logo modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="./vercel.svg"
-              alt="Vercel Logo"
+              src="./tekgrp-logo.png"
+              alt="tekgrp Logo"
               className="dark:invert"
               width={100}
               height={24}
